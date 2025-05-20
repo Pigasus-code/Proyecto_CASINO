c9c01d05777ba5a6028900ab31f10830dd74ad5b
juan_ramirez <jjramirezvasquez2007@gmail.com>
2025-05-20 12:33:02 -0500

    completando los modulos de contador, usuario, cuadre maquina y cuadre casino

--- back/src/CuadreMaquina/GestorCuadreMaquina.py
+++ back/src/CuadreMaquina/GestorCuadreMaquina.py
@@ -1,4 +1,5 @@
 from back.src.CuadreMaquina.CuadreMaquina import CuadreMaquina
+from util import GestorArchivos
 
 class GestorCuadreMaquina:
     
@@ -7,28 +8,37 @@ class GestorCuadreMaquina:
     
 
     def calculo_total_contadores(self,fecha_inicio,fecha_fin,asset_maquina,lista_contadores)->tuple:
-        """
-        se debe retornar una tupla con el calculo de los contadores (in,out,jackpot,billetero)
-        de una maquina en una rango de fechas. La formula para este calculo se encuentra
-        en el documento del proyecto
-        """
-        pass
+        contadores=list(filter(lambda contador: contador.maquina.asset==asset_maquina and (contador.fecha==fecha_inicio or contador.fecha==fecha_fin),lista_contadores))
+        if len(contadores)!=2:
+            return None
+        contadores.sort(key=lambda contador: contador.fecha)
+        inicial,final=contadores
+        denominacion=inicial.maquina.denominacion
+        lista1=[inicial.in_,inicial.out,inicial.jackpot,inicial.billetero]
+        lista2=[final.in_,final.out,final.jackpot,final.billetero]
+        ans=[(j-i)*denominacion for i,j in zip(lista1,lista2)]
+        return tuple(map(lambda x: round(x,1),ans))
+        
     
     def calculo_utilidad_maquina(self,fecha_inicio,fecha_fin,asset_maquina,lista_contadores)->float:
-        """
-        se debe retornar el calculo de la utilidad de una mquina en una rango de fecha,
-        formula para realizar este calculo se encuentra en el documento del proyecto
-        """
-        pass
+        contadores=list(filter(lambda contador: contador.maquina.asset==asset_maquina and (contador.fecha==fecha_inicio or contador.fecha==fecha_fin),lista_contadores))
+        if len(contadores)!=2:
+            return None
+        contadores.sort(key=lambda contador: contador.fecha)
+        inicial,final=contadores
+        utilidad = (final.in_-inicial.in_) - ((final.out -inicial.out)+ ( final.jackpot-inicial.jackpot))
+        return round(utilidad,1)
 
-    def guardar_resultados(self,contadores:tuple,utilidad:float,maquina:object):
-        """
-        se debe agregar a la lista cuedre_maquina un objeto de tipo cuadre maquina
-        y mandarle los atributos de cada contador (in,out,jackpot,billetero) que se resiven
-        en tupla, la utilidad de la maquina y el objeto. Tambien se debe agregar esta 
-        informacion en el archivo csv para que quede un registro en la base de datos
-        """
-        pass
+    def guardar_resultados(self,contadores:tuple,utilidad:float,maquina:object)->bool:
+        if not contadores or not utilidad:
+            return False
+        try:
+            in_,out,jackpot,billetero=contadores
+            GestorArchivos.escribir_csv("CASINO/Data/CuadrePorMaquina.csv",[{"in":in_,"out":out,"jackpot":jackpot,"billetero":billetero,"utilidad":utilidad,"maquina":maquina.asset}])
+            self.__cuadre_maquina.append(CuadreMaquina(in_,out,jackpot,billetero,utilidad,maquina))
+            return True
+        except:
+            return False
     
     def lista_cuadre_quina(self)->list:
         return self.__cuadre_maquina
